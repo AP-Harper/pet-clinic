570ba432d50c424d129163bea9c13c8697728b1d
AP-Harper <142895139+AP-Harper@users.noreply.github.com>
2024-04-26 14:36:34 +0100

    Added Pets and Contact Ingo to owners on startup

--- pet-clinic-web/src/main/java/andrew/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/andrew/bootstrap/DataLoader.java
@@ -1,31 +1,56 @@
 package andrew.bootstrap;
 
 import andrew.model.Owner;
+import andrew.model.Pet;
+import andrew.model.PetType;
 import andrew.model.Vet;
 import andrew.services.OwnerService;
+import andrew.services.PetTypeService;
 import andrew.services.VetService;
 import andrew.services.map.OwnerServiceMap;
 import andrew.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Andrew");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Sauchiehall Street");
+        owner1.setCity("Glasgow");
+        owner1.setTelephone("0141 667 6767");
+
+        Pet andrewPet = new Pet();
+        andrewPet.setPetType(savedDogPetType);
+        andrewPet.setOwner(owner1);
+        andrewPet.setBirthDate(LocalDate.now());
+        andrewPet.setName("Jeff");
+        owner1.getPets().add(andrewPet);
 
         ownerService.save(owner1);
 
@@ -33,6 +58,16 @@ class DataLoader implements CommandLineRunner {
         Owner owner2 = new Owner();
         owner2.setFirstName("John");
         owner2.setLastName("Jerry");
+        owner1.setAddress("123 Buchanan Street");
+        owner1.setCity("Glasgow");
+        owner1.setTelephone("0141 667 4422");
+
+        Pet JohnCat = new Pet();
+        JohnCat.setName("Brian");
+        JohnCat.setPetType(savedCatPetType);
+        JohnCat.setBirthDate(LocalDate.now());
+        JohnCat.setOwner(owner2);
+        owner1.getPets().add(JohnCat);
 
         ownerService.save(owner2);
 
